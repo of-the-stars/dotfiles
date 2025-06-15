1bd748883ac0c1d0c72e6c13d07938fe69859b12
internet_wizard <umex28@gmail.com>
2025-06-14 19:37:28 -0500

    Modified rebuild.sh to not fuck things up with stow

--- rebuild.sh
+++ rebuild.sh
@@ -2,17 +2,11 @@
 set -e
 pushd ~/dotfiles/nixos/
 nvim configuration.nix
-echo "NixOS Rebuilding..."
-stow .
-git diff -U1 configuration.nix
+# alejandra . &>/dev/null
+git diff -U0 configuration.nix
 sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | rg --color error && false)
+echo "NixOS Rebuilding..."
 gen=$(nixos-rebuild list-generations | rg current)
 git commit -a -m "$gen"
 popd
-# alejandra . &>/dev/null
-# git diff -U0 *.nix
-# sudo nixos-rebuild switch &>nixos-switch.log || (
-#  cat nixos-switch.log | grep --color error && false)
-# gen=$(nixos-rebuild list-generations | grep current)
-# 
 
