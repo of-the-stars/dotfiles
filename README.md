d981cf4468519d43fd7fd3d12c0e632dcd29e015
internet_wizard <umex28@gmail.com>
2025-06-14 21:40:06 -0500

    Filled out `README.md` and added `fastfetch-latest`
    `./../rebuild.sh` will now run `fastfetch` and store the result in
    `fastfetch-latest`.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotfiles
+
+![[./fastfetch-latest]]
+
+My personal dotfiles! My main machine is a Thinkpad T14 Gen 5, currently running NixOS with Hyprland.
+
+Above is my latest result from `fastfetch`, which runs each time after I rebuild NixOS.
--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# Based off of `0atman's` rebuild script, the script will let you configure NixOS, then rebuild and `git commit` for you
+
 set -e
 pushd ~/dotfiles/nixos/
 nvim configuration.nix
@@ -8,5 +11,6 @@ echo "\nNixOS Rebuilding...\n"
 sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | rg --color error && false)
 gen=$(nixos-rebuild list-generations | rg current)
 git commit -a -m "$gen"
+fastfetch &>./../fastfetch-latest
 popd
 
