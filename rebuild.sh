f4ff2910417ef76b67348cb065d4b88c370fe770
internet_wizard <umex28@gmail.com>
2025-06-14 19:39:08 -0500

    Changed the placement of the nixos rebuilding message

--- rebuild.sh
+++ rebuild.sh
@@ -4,8 +4,8 @@ pushd ~/dotfiles/nixos/
 nvim configuration.nix
 # alejandra . &>/dev/null
 git diff -U0 configuration.nix
+echo "\nNixOS Rebuilding...\n"
 sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | rg --color error && false)
-echo "NixOS Rebuilding..."
 gen=$(nixos-rebuild list-generations | rg current)
 git commit -a -m "$gen"
 popd
