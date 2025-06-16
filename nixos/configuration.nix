3034f3c02c0fb6dea637e887b87050b0e7020ec2
internet_wizard <umex28@gmail.com>
2025-06-16 00:32:41 -0500

    30 current  2025-06-16 00:32:38  25.05.804002.5f4f306bea96  6.12.33                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -155,6 +155,19 @@
     };
   };
 
+  programs.obs-studio = {
+    enable = true;
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
   # Enable starship
   programs.starship = {
     enable = true;
