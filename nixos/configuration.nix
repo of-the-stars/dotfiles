3083386c1e784b9063378c52814920261f244a57
internet_wizard <umex28@gmail.com>
2025-06-29 15:38:00 -0500

    77 current  2025-06-29 15:37:55  25.05.804745.7284e2decc98  6.12.34                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -227,6 +227,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    fzf
     yazi
     rustup
     rust-analyzer
