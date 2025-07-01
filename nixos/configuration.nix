67eb3a6397b1369efe0360c9bcb3cb1ca7ea9e4a
internet_wizard <umex28@gmail.com>
2025-06-30 20:34:43 -0500

    88 current  2025-06-30 20:34:38  25.05.805252.b43c397f6c21  6.12.35                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -233,6 +233,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    ffmpeg
     clang
     clang-tools
     usbutils
