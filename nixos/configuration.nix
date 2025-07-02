caf49a55ac47977e27bb5185c267333b27ace5a2
internet_wizard <umex28@gmail.com>
2025-07-01 21:12:48 -0500

    91 current  2025-07-01 21:12:42  25.05.805252.b43c397f6c21  6.12.35                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -234,6 +234,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    spotify
     ffmpeg
     clang
     clang-tools
