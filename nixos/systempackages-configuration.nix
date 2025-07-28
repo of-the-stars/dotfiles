c0b9f639f0027de3bddfcc16a4f90511b23004dc
internet_wizard <umex28@gmail.com>
2025-07-28 10:43:54 -0500

    120 current  2025-07-28 10:43:52  25.05.807117.13e8d35b7d60  6.12.40                          *

--- nixos/systempackages-configuration.nix
+++ nixos/systempackages-configuration.nix
@@ -6,6 +6,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    element-desktop
     neovim
     lua-language-server
     v4l-utils
