d31020baa8d83e892aa5d6c51f1978d625c850fe
internet_wizard <umex28@gmail.com>
2025-08-20 01:17:21 -0500

    146 current  2025-08-20 01:17:18  25.05.808437.48f4c982de68  6.12.42                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -62,7 +62,7 @@
   # Enable tlp for laptop power management
   services.tlp = {
     enable = true;
-    # settings = {
+      settings = {
     #   CPU_SCALING_GOVERNOR_ON_AC = "performance";
     #   CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
     #
@@ -75,9 +75,9 @@
     #   CPU_MAX_PERF_ON_BAT = 20;
     #
     #   #Optional helps save long term battery health
-    #   START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-    #   STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-    # };
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
   };
 
   # Set your time zone.
