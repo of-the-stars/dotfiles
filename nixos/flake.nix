7a067888066a7e7202f1ba5c76ca8240ab1d6698
internet_wizard <umex28@gmail.com>
2025-08-21 22:10:51 -0500

    Moved the nvim flake into the nixos directory

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     
     # custom nvim configuration flake
-    nvim.url = "github:internetwiz4rd/nvim-config/master"
+    nvim.url = "path:./nvim.nix"
   };
 
   outputs = inputs@{ self, nixpkgs, ... }: {
