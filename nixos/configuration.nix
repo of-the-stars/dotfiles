d7e19b74b4e0bba25ec06e977f85411cfa465fd0
internet_wizard <umex28@gmail.com>
2025-08-28 12:25:00 -0500

    160 current  2025-08-28 12:24:57  25.05.808864.4e7667a90c16  6.12.43                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -163,7 +163,7 @@
     # Optional:
     network.listenAddress = "any"; # if you want to allow non-localhost connections
     # network.startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
-    user = "internet_wizard";
+    # user = "internet_wizard";
   };
 
   systemd.services.mpd.environment = {
