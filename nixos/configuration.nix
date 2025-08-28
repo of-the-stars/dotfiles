7eae9636dfd3e8f005d5213026ca731212adaaae
internet_wizard <umex28@gmail.com>
2025-08-27 21:56:05 -0500

    152 current  2025-08-27 21:56:02  25.05.808864.4e7667a90c16  6.12.43                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -156,10 +156,10 @@
     user = "internet_wizard";
   };
 
-  systemd.services.mpd.environment = {
-    # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-    XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.internet_wizard.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
-  };
+  # systemd.services.mpd.environment = {
+  #   # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
+  #   XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.internet_wizard.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
+  # };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.internet_wizard = {
