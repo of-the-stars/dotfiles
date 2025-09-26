9d3610cf745e215c7424d5d38e3f36cfaeae6803
internet_wizard <umex28@gmail.com>
2025-09-26 16:40:16 -0500

    This whole thing was cuz I forgot a comma FML

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -445,15 +445,15 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
       vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
-      vim.keymap.set('n', '<leader>ff', builtin.find_files { hidden = true }, { desc = '[F]ind [F]iles' })
-      -- vim.keymap.set('n', '<leader>fF', builtin.find_files { hidden = true }, { desc = '[F]ind [F]iles (including hidden files)' })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { hidden = true }, { desc = '[F]ind [F]iles' })
+      vim.keymap.set('n', '<leader>fF', builtin.find_files, { hidden = true }, { desc = '[F]ind [F]iles (including hidden files)' })
       vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]elect Telescope' })
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind in current [W]ord' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
       vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-      -- vim.keymap.set('n', '<leader>ft', builtin.treesitter, { desc = '[F]ind in [T]reesitter' })
+      vim.keymap.set('n', '<leader>ft', builtin.treesitter, { desc = '[F]ind in [T]reesitter' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
