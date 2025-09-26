eaeffca84dcd998b6cdb823e710e02043292b0df
internet_wizard <umex28@gmail.com>
2025-09-26 16:59:21 -0500

    Added checks for rebuild to see if nvim was changed

--- rebuild.sh
+++ rebuild.sh
@@ -8,7 +8,12 @@ nvim configuration.nix
 # alejandra . &>/dev/null
 git diff -U0 *.nix
 echo "NixOS Rebuilding..."
-sudo nix flake update nvim
+
+# Checks if the nvim directory was changed, then updates the flake so that the changes are reflected
+if git diff --cached --name-only --relative=./../.config/nvim/ | rg -q "."; then
+    sudo nix flake update nvim
+fi
+
 sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | rg --color=always error && false)
 gen=$(nixos-rebuild list-generations | rg current)
 git commit -a -m "$gen"
