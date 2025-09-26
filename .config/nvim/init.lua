cebca2c98de6dbcee5ec88700571ae53189ec1bd
internet_wizard <umex28@gmail.com>
2025-09-26 17:02:08 -0500

    225 current  2025-09-26 17:02:06  25.05.20250925.25e53aa  6.12.48                          *

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -448,7 +448,7 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
       vim.keymap.set('n', '<leader>fF', function()
         builtin.find_files { hidden = true }
-      end, { desc = '[F]ind [F]iles (including hidden files)' })
+      end, { desc = '[F]ind hidden [F]iles ' })
       vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]elect Telescope' })
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind in current [W]ord' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
