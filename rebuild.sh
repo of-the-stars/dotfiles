4d20f5acc9a51d0e8225bb6100dfa4d3a2c8002a
internet_wizard <umex28@gmail.com>
2025-09-27 01:07:45 -0500

    227 current  2025-09-27 01:07:42  25.05.20250925.25e53aa  6.12.48                          *

--- rebuild.sh
+++ rebuild.sh
@@ -4,17 +4,20 @@
 
 set -e
 pushd ~/dotfiles/nixos/
-nvim configuration.nix
+
+nvim
 # alejandra . &>/dev/null
+#
 git diff -U0 *.nix
 echo "NixOS Rebuilding..."
 
 # Checks if the nvim directory was changed, then updates the flake so that the changes are reflected
-#if git diff --cached --name-only --relative=./../.config/nvim/ | rg -q "."; then
+if git diff --cached --name-only ./../.config/nvim/. | rg -q "."; then
     sudo nix flake update nvim
-#fi
+fi
 
 sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | rg --color=always error && false)
 gen=$(nixos-rebuild list-generations | rg current)
 git commit -a -m "$gen"
+
 popd
