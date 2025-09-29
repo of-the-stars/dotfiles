39316ec0dd03c65b8e8ff3c6b7a18625e4a6182e
internet_wizard <umex28@gmail.com>
2025-09-29 00:37:07 -0500

    237 current  2025-09-29 00:37:04  25.05.20250925.25e53aa  6.12.48                          *

--- rebuild.sh
+++ rebuild.sh
@@ -16,14 +16,10 @@ pushd ~/dotfiles/nixos/
 git diff -U0 *.nix
 echo "NixOS Rebuilding..."
 
-    # Checks if the nvim directory was changed, then updates the flake so that the changes are reflected
-    pushd ~/dotfiles/.config/nvim/
-
-    if git diff --cached --name-only . | rg -q "."; then
-        sudo nix flake update nvim
-    fi
-
-    popd
+# Checks if the nvim directory was changed, then updates the flake so that the changes are reflected
+if git diff --cached --name-only ./../.config/nvim/. | rg -q "."; then
+    sudo nix flake update nvim
+fi
 
 sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | rg --color=always error && false)
 gen=$(nixos-rebuild list-generations | rg current)
