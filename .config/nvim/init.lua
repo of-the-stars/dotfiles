431d64927c8807896636f51a5fca99c45e3f4d81
internet_wizard <umex28@gmail.com>
2025-10-04 10:52:05 -0500

    285 current  2025-10-04 10:52:01  25.05.20251001.5b5be50  6.12.49                          *

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -847,7 +847,7 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       -- C-k: Toggle signature help (if signature.enabled = true)
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
+      keymap = { preset = 'enter' },
 
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
