47b32d8559a60f88aec9f41a75b1370a03699a77
internet_wizard <umex28@gmail.com>
2025-10-04 10:34:35 -0500

    283 current  2025-10-04 10:34:30  25.05.20251001.5b5be50  6.12.49                          *

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -570,9 +570,9 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      --[[ local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      vim.lsp.config('*', { capabilities = capabilities })
+      vim.lsp.config('*', { capabilities = capabilities }) ]]
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
