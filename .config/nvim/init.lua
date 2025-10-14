4ad7dfa6ff5044149eff00668279bc34bb919539
internet_wizard <umex28@gmail.com>
2025-10-14 14:47:17 -0500

    374 current  2025-10-14 14:47:08  25.05.20251001.5b5be50  6.12.49                          *

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -999,6 +999,13 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {
+      size = function(term)
+        if term.direction == 'horizontal' then
+          return 15
+        elseif term.direction == 'vertical' then
+          return vim.o.columns * 0.4
+        end
+      end,
       open_mapping = [[<leader>t]],
       insert_mappings = false,
       terminal_mappings = false,
