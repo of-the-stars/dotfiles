ff7865f7f324f504333898811ee3cb5a10ed2b3d
internet_wizard <>
2025-10-28 20:47:55 -0500

    432 current  2025-10-28 20:47:43  25.05.20251001.5b5be50  6.12.49                          *

--- nixos/han-tyumi/configuration.nix
+++ nixos/han-tyumi/configuration.nix
@@ -125,29 +125,22 @@ in {
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  # services.pulseaudio.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-    "monitor.bluez.properties" = {
-      "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = true;
-      "bluez5.enable-hw-volume" = true;
-      "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
-    };
-  };
+  # services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+  #   "monitor.bluez.properties" = {
+  #     "bluez5.enable-sbc-xq" = true;
+  #     "bluez5.enable-msbc" = true;
+  #     "bluez5.enable-hw-volume" = true;
+  #     "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+  #   };
+  # };
 
   catppuccin.enable = true;
 
@@ -167,13 +160,9 @@ in {
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     # Add missing dynamic libraries for unpackaged programs here
-    icu
-    stdenv.cc.cc
   ];
 
   environment.localBinInPath = true;
-
-  environment.variables.PATH = "${pkgs.clang-tools}/bin:$PATH";
 
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
@@ -193,11 +182,11 @@ in {
   # List services that you want to enable:
 
   # VR shit
-  services.monado.enable = true;
-  programs.alvr = {
-    enable = true;
-    openFirewall = true;
-  };
+  # services.monado.enable = true;
+  # programs.alvr = {
+  #   enable = true;
+  #   openFirewall = true;
+  # };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
