7cd646b08e320acf0f924a2df330e317d596d791
of-the-stars <stellae.incaelis@gmail.com>
2025-11-05 21:15:25 -0600

    456 current  2025-11-05 21:15:15  25.05.20251001.5b5be50  6.12.49                          *

--- nixos/han-tyumi/configuration.nix
+++ nixos/han-tyumi/configuration.nix
@@ -128,9 +128,12 @@ in {
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
+    jack.enable = true;
   };
 
   # services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
