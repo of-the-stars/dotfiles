664e4b18d9f6323ff4b39a730873b1a40876f12c
of-the-stars <stellae.incaelis@gmail.com>
2025-11-05 18:42:48 -0600

    453 current  2025-11-05 18:23:22  25.05.20251001.5b5be50  6.12.49                          *

--- nixos/han-tyumi/configuration.nix
+++ nixos/han-tyumi/configuration.nix
@@ -19,6 +19,14 @@ in {
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [gnome-tour gnome-user-docs];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
