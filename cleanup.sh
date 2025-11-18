fd925567f2f5b0581fc919dbe4171a804b614030
of-the-stars <stellae.incaelis@gmail.com>
2025-11-18 15:07:59 -0600

    479 current  2025-11-18 15:07:56  25.05.20251001.5b5be50  6.12.49                          *

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# Stolen from the 11th nix pill by Lethalman
+# Stolen and modified from the 11th nix pill by Lethalman
 
-nix-channel --update
-nix-env -u --always
 sudo rm /nix/var/nix/gcroots/auto/*
 sudo nix-collect-garbage -d
-sudo nixos-rebuild switch
+./rebuild.sh
