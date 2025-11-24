d4418bf4e002a0db815bded78cc610cc7e1d2176
of-the-stars <stellae.incaelis@gmail.com>
2025-11-23 22:16:52 -0600

    507 current  2025-11-23 22:16:51  25.05.20251001.5b5be50  6.12.49                          *
    Added PAM via hardware keys

--- nixos/nixos-modules/hardware-security.nix
+++ nixos/nixos-modules/hardware-security.nix
@@ -8,32 +8,34 @@
   imports = [
     # Paths to other modules.
     # Compose this module out of smaller ones.
-    ./terminal.nix
-    ./hyprland-config.nix
-    ./media-tools.nix
-    ./networking-tools.nix
-    ./virtual-machines.nix
-
   ];
 
   options = {
     # Option declarations.
     # Declare what settings a user of this module can set.
     # Usually this includes a global "enable" option which defaults to false.
+    hardware-security.enable = lib.mkEnableOption "Enable rigorous authentication";
   };
 
-  config = {
+  config = lib.mkIf config.hardware-security.enable {
     # Option definitions.
     # Define what other settings, services and resources should be active.
     # Usually these depend on whether a user of this module chose to "enable" it
     # using the "option" above.
     # Options for modules imported in "imports" can be set here.
 
-    terminal.enable = lib.mkDefault true;
-    hyprland-config.enable = lib.mkDefault true;
-    media-tools.enable = lib.mkDefault true;
-    networking-tools.enable = lib.mkDefault true;
+    # Security using yubikey
+    services.udev.packages = [ pkgs.yubikey-personalization ];
 
-    virtual-machines.enable = lib.mkDefault false;
+    programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      yubikey-manager
+      yubikey-manager-qt
+      yubikey-personalization-gui
+    ];
   };
 }
--- nixos/nixos-modules/virtual-machines.nix
+++ nixos/nixos-modules/virtual-machines.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ...}:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   imports = [
     # Paths to other modules.
@@ -9,22 +14,21 @@
     # Option declarations.
     # Declare what settings a user of this module can set.
     # Usually this includes a global "enable" option which defaults to false.
-    virtual-machines.enable =
-      lib.mkEnableOption "Enable virtual machine tools";
+    virtual-machines.enable = lib.mkEnableOption "Enable virtual machine tools";
   };
 
   config = lib.mkIf config.virtual-machines.enable {
     # Option definitions.
     # Define what other settings, services and resources should be active.
     # Usually these depend on whether a user of this module chose to "enable" it
-    # using the "option" above. 
+    # using the "option" above.
     # Options for modules imported in "imports" can be set here.
 
     virtualisation.virtualbox.host.enable = true;
-    users.extraGroups.vboxusers.members = ["internet_wizard"];
+    users.extraGroups.vboxusers.members = [ "internet_wizard" ];
 
     programs.virt-manager.enable = true;
-    users.groups.libvirtd.members = ["internet_wizard"];
+    users.groups.libvirtd.members = [ "internet_wizard" ];
     virtualisation.libvirtd.enable = true;
     virtualisation.spiceUSBRedirection.enable = true;
   };
