d6a6bf1c4fc6b6e7ed27d8f1f526edf8c57cee99
of-the-stars <stellae.incaelis@gmail.com>
2025-12-03 01:05:34 -0600

    563 2025-12-03 01:05:31 25.11.20251130.d542db7 6.12.59

--- .config/nvim/lua/custom/plugins/tidal-cycles.lua
+++ .config/nvim/lua/custom/plugins/tidal-cycles.lua
@@ -2,14 +2,13 @@ return {
   {
     'tidalcycles/vim-tidal',
     enabled = require('nixCatsUtils').enableForCategory 'tidal-cycles',
-    event = 'VimEnter',
-    vim.api.nvim_create_autocmd('VimEnter', {
-      group = vim.api.nvim_create_augroup('autocom', { clear = true }),
-      pattern = '*.tidal',
-      callback = function()
-        vim.fn.system 'superdirt-start'
-      end,
-    }),
+    --vim.api.nvim_create_autocmd('VimEnter', {
+    --  group = vim.api.nvim_create_augroup('autocom', { clear = true }),
+    --  pattern = '*.tidal',
+    --  callback = function()
+    --    vim.fn.system 'superdirt-start'
+    --  end,
+    --}),
     config = function()
       -- vim.keymap.set({ 'n', 'v' }, '<leader>ms', '<cmd>TidalSend<cr>', {
       --   desc = '[S]end line(s) to Tidal',
