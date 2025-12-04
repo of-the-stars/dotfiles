b28dafb68e5cd641749bf555ae51781e4ac446f4
of-the-stars <stellae.incaelis@gmail.com>
2025-12-04 14:19:40 -0600

    573 2025-12-04 14:19:38 25.11.20251130.d542db7 6.12.59

--- .config/nvim/lua/custom/plugins/tidal-cycles.lua
+++ .config/nvim/lua/custom/plugins/tidal-cycles.lua
@@ -2,36 +2,18 @@ return {
   {
     'tidalcycles/vim-tidal',
     enabled = require('nixCatsUtils').enableForCategory 'tidal-cycles',
-    -- vim.api.nvim_create_autocmd('VimEnter', {
-    --  group = vim.api.nvim_create_augroup('autocom', { clear = true }),
-    --  pattern = '*.tidal',
-    --  callback = function()
-    --    vim.fn.system 'superdirt-start'
-    --  end,
-    -- }),
     event = 'VimEnter',
     config = function()
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>ms', '<cmd>TidalSend<cr>', {
-      --   desc = '[S]end line(s) to Tidal',
-      -- })
-      --
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>mh', '<cmd>TidalHush<cr>', {
-      --   desc = '[H]ush Tidal',
-      -- })
-
-      -- vim.keymap.set({ 'n', 'v' }, '<CTRL><CR>', '<cmd>TidalHush<cr>', {
-      --   desc = '[H]ush Tidal',
-      -- })
-
-      -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufReadPost' }, {
-      --   group = vim.api.nvim_create_augroup('SuperDirt', { clear = true }),
-      --   pattern = { '*.tidal' },
-      --   callback = function()
-      -- vim.fn.jobstart({ 'superdirt-start' }, {})
-      --   end,
-      -- })
-      --
-      vim.system { 'superdirt-start' }
+      local on_exit = function(obj)
+        print(obj.code)
+        print(obj.signal)
+        print(obj.stdout)
+        print(obj.stderr)
+      end
+      local superdirt = vim.system({ 'superdirt-start' }, {
+        detach = false,
+      }, on_exit)
+      print('Superdirt Started; PID: ' .. superdirt.pid)
     end,
   },
 }
