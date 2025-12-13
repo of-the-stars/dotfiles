4c867f946002012683fdbe9a2d5dd1826a1685e2
of-the-stars <stellae.incaelis@gmail.com>
2025-12-13 13:53:45 -0600

    609 2025-12-13 13:53:42 25.11.20251130.d542db7 6.12.59

--- .config/nvim/lua/kickstart/plugins/lint.lua
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -8,7 +8,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
         nix = { 'nix' },
       }
 
