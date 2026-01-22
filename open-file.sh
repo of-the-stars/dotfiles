750327bee433073563fd5421f31f0e912ba11334
of-the-stars <stellae.incaelis@gmail.com>
2026-01-22 17:37:59 -0600

    664 2026-01-22 17:37:56 25.11.20251130.d542db7 6.17.9

--- open-file.sh
+++ open-file.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-FILE="$(eza -1 --absolute=follow -R -D | rofi -dmenu)" 
-FILE+="/$(eza -1 -f --sort=accessed "$FILE" | rofi -dmenu)"
+FILE=$(eza -1 --absolute=follow -R -D | rofi -dmenu) 
+FILE+=/$(eza -1 -f --sort=accessed "$FILE" | rofi -dmenu)
 xdg-open "${FILE}"
