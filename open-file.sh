91cd6e7815c5ed3da8576ec4313d87c6c6adb156
of-the-stars <stellae.incaelis@gmail.com>
2026-01-25 16:34:23 -0600

    681 2026-01-25 16:34:19 25.11.20251130.d542db7 6.17.9

--- open-file.sh
+++ open-file.sh
@@ -2,6 +2,7 @@
 
 FILE=$(fd -a --type=d | rofi -i -dmenu -p "Choose Folder") 
 echo "$FILE"
-FILE+=$(eza -1 -f --sort=accessed --no-quotes "$FILE" | rofi -i -dmenu -p "Choose File")
+z "$FILE"
+FILE+=$(eza -1 -f --sort=accessed --no-quotes | rofi -i -dmenu -p "Choose File")
 echo "$FILE"
 handlr open "${FILE}"
