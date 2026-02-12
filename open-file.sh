27ee9d67cf281a541e7a726c0f5fdbab1d765789
of-the-stars <stellae.incaelis@gmail.com>
2026-02-12 13:03:54 -0600

    han-tyumi 730 2026-02-12 13:03:51 25.11.20260128.fa83fd8 6.18.7

--- open-file.sh
+++ open-file.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 FILE=$(fd -a --type=d | rofi -i -dmenu -p "Choose Folder") 
-echo "$FILE"
+# echo "$FILE"
 cd "$FILE" || exit
-FILE+=$(eza -1 -f --sort=accessed --no-quotes | rofi -i -dmenu -p "Choose File" -matching fuzzy)
-echo "$FILE"
+FILE+=$(eza -1 -f --sort=accessed --no-quotes "$FILE" | rofi -i -dmenu -p "Choose File" -matching fuzzy)
+# echo "$FILE"
 handlr open "${FILE}"
