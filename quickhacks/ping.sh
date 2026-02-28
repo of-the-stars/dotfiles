af53285f42c4161c700aaa241791adb303047cdb
of-the-stars <stellae.incaelis@gmail.com>
2026-02-27 18:41:39 -0600

    han-tyumi 800 2026-02-27 18:41:35 25.11.20260216.fa56d7d 6.19.0

--- quickhacks/ping.sh
+++ quickhacks/ping.sh
@@ -2,6 +2,7 @@
 
 temp="$(mktemp -t "ping-result.XXXXXX")"
 IP_ADDR="$(ip address | rg inet | awk '{print $2}' | sed '3q;d')"
+echo "IP Address: $IP_ADDR"
 nmap -sL -A -v "$IP_ADDR" | tee "$temp" | bat --paging=never -pl log
 nvim "$temp"
 rm -f -- "$temp"
