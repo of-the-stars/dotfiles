9ad26fc796c1c08b001fb570e9743fb01dde6f86
of-the-stars <stellae.incaelis@gmail.com>
2026-03-02 11:08:08 -0600

    han-tyumi 821 2026-03-02 11:08:05 25.11.20260216.fa56d7d 6.19.0

--- spellbook/knock-knock.sh
+++ spellbook/knock-knock.sh
@@ -3,6 +3,6 @@
 temp="$(mktemp -t "knock-knock-result.XXXXXX")"
 IP_ADDR="$(ip address | rg inet | awk '{print $2}' | sed '3q;d')"
 echo "IP Address: $IP_ADDR"
-nmap -sL -T4 "$IP_ADDR" | rg -v '^Nmap scan report for \d+\.\d+\.\d+\.\d+$' | tee "$temp"
+nmap -sL -T4 -v "$IP_ADDR" | rg -v '^Nmap scan report for \d+\.\d+\.\d+\.\d+$' | tee "$temp"
 echo
 echo "knock-knock result stored at $temp"
