d9fcc823cfd39373e635cac3d3006876f3bb22e1
of-the-stars <stellae.incaelis@gmail.com>
2026-03-01 22:50:20 -0600

    han-tyumi 820 2026-03-01 22:50:17 25.11.20260216.fa56d7d 6.19.0

--- spellbook/whos-there.sh
+++ spellbook/whos-there.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
-# temp="$(mktemp -t "ping-result.XXXXXX")"
+temp="$(mktemp -t "whos-there-result.XXXXXX")"
 # IP_ADDR="$(ip address | rg inet | awk '{print $2}' | sed '3q;d')"
 # echo "IP Address: $IP_ADDR"
 # nmap -sL -v -A -T3 "$IP_ADDR" | tee "$temp" | bat --paging=never -pl log
 # "$EDITOR" "$temp"
-# rm -f -- "$temp"
+
+STDIN="$(cat)"
+
+door_knocked="$(echo "$STDIN" | rg -q 'Starting Nmap')" 
+
+if $door_knocked; then
+    ip_list="$(echo "$STDIN" | rg 'Nmap scan report for' | awk '{print $5, $6}' | column -t )"
+else
+    ip_list=$STDIN
+fi
+
+echo "Runnin investigation on:"
+echo "$ip_list" 
+echo
+
+nmap -iL <(echo "$ip_list" | sd '.+\((.+)\)$' \$1) -v -A -T4 | tee "$temp"
+echo
+echo "whos-there result stored at $temp"
