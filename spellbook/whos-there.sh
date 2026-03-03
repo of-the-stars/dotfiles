0f2756052a0fc946cb40b40f7e30f546d6f1913a
of-the-stars <stellae.incaelis@gmail.com>
2026-03-02 22:17:31 -0600

    han-tyumi 822 2026-03-02 22:17:27 25.11.20260216.fa56d7d 6.19.0

--- spellbook/whos-there.sh
+++ spellbook/whos-there.sh
@@ -4,18 +4,20 @@ temp="$(mktemp -t "whos-there-result.XXXXXX")"
 
 STDIN="$(cat)"
 
-door_knocked="$(echo "$STDIN" | rg -q 'Starting Nmap')" 
-
-if $door_knocked; then
+if echo "$STDIN" | rg -q 'Starting Nmap'; then
+    # If `knock-knock` or another invocation of `nmap -sL` was piped into it
     ip_list="$(echo "$STDIN" | rg 'Nmap scan report for' | awk '{print $5, $6}' | column -t )"
+    echo "Runnin investigation on:"
+    echo "$ip_list" 
+    echo
+    ip_list="$(echo "$ip_list" | sd '.+\((.+)\)$' \$1)"
 else
-    ip_list=$STDIN
+    ip_list="$STDIN"
+    echo "Runnin investigation on:"
+    echo "$ip_list" 
+    echo
 fi
 
-echo "Runnin investigation on:"
-echo "$ip_list" 
-echo
-
-nmap -iL <(echo "$ip_list" | sd '.+\((.+)\)$' \$1) -v -A -T4 | tee "$temp"
+nmap -iL <(echo "$ip_list") -A -T4 | tee "$temp"
 echo
 echo "whos-there result stored at $temp"
