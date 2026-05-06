eac752480f518c134d11e0fe0cf46953faf1bb5e
of-the-stars <stellae.incaelis@gmail.com>
2026-05-06 09:53:12 -0500

    han-tyumi 845 2026-05-06 09:53:10 25.11.20260302.c581273 6.19.5

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -854,11 +854,11 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
     -- I'm testing my build script here
 
     -- use a release tag to download pre-built binaries
-    version = '1.*',
+    -- version = '1.*',
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     -- build = 'cargo build --release',
     -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+    build = 'nix run .#build-plugin',
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
