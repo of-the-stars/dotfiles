47d5f1da003aed3cec26502abd64c2019ec9dd25
of-the-stars <stellae.incaelis@gmail.com>
2026-05-06 13:15:26 -0500

    han-tyumi 847 2026-05-06 13:15:24 25.11.20260302.c581273 6.19.5

--- spellbook/dnd.sh
+++ spellbook/dnd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # This assumes you're using Dunst as your notification daemon
 
-echo "true false" | rofi -sep ' ' -dmenu -p "Switch Do Not Disturb on or off" | xargs dunstctl set-paused 
+echo "true false" | rofi -sep ' ' -dmenu -p "Set Do Not Disturb On?" | xargs dunstctl set-paused 
