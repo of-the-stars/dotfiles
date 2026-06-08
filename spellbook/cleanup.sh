b094bea01c14da00d6f7c0406c282a7eced5b969
of-the-stars <stellae.incaelis@gmail.com>
2026-06-08 14:59:24 -0500

    871 2026-06-08 14:59:20 26.05.20260528.ec942ba 7.0.10

--- spellbook/cleanup.sh
+++ spellbook/cleanup.sh
@@ -3,5 +3,7 @@
 # Stolen and modified from the 11th nix pill by Lethalman
 
 # sudo rm /nix/var/nix/gcroots/auto/*
+SCRIPT_DIRECTORY="$(dirname $(realpath "$0"))"
+
 sudo nix-collect-garbage -d
-. ./rebuild.sh
+./"$SCRIPT_DIRECTORY"/rebuild.sh
