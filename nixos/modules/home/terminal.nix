f5e766ed3132adcc98410d1166046b7a11b76429
of-the-stars <stellae.incaelis@gmail.com>
2026-07-01 10:43:10 -0500

    940 2026-07-01 10:43:06 26.05.20260611.a037402 7.1.1

--- nixos/modules/home/terminal.nix
+++ nixos/modules/home/terminal.nix
@@ -40,21 +40,34 @@
       };
 
       initContent = ''
-        ZSH_THEME=""
+          ZSH_THEME=""
 
-        bindkey -v
-        bindkey '^E' autosuggest-accept
-        bindkey -M vicmd ' ' edit-command-line
+          bindkey -v
+          bindkey '^E' autosuggest-accept
+          bindkey -M vicmd ' ' edit-command-line
 
-        export MANPAGER="nvim +Man!"
+          export MANPAGER="nvim +Man!"
 
-        # fortune-kind | tee ~/fortune.txt | cowsay -s -f bong | tee ~/cowsay.txt
+          # fortune-kind | tee ~/fortune.txt | cowsay -s -f bong | tee ~/cowsay.txt
 
-        # To remind myself what it's all about
-        # Snippet to use fd to find the file, in case I move the file
-        # fd --base-directory=Log4Stell 2026\ Resolutions
+          # To remind myself what it's all about
+          # Snippet to use fd to find the file, in case I move the file
+          # fd --base-directory=Log4Stell 2026\ Resolutions
 
-        # sed '1 { /^---/ { :a N; /\n---/! ba; d} }' ~/Log4Stell/02-Permanent/2026-01-02T0118\ 2026\ Resolutions.md | bat -l=md -p
+          # sed '1 { /^---/ { :a N; /\n---/! ba; d} }' ~/Log4Stell/02-Permanent/2026-01-02T0118\ 2026\ Resolutions.md | bat -l=md -p
+
+        # enable starship
+        eval "$(starship init zsh)"
+
+        # enable zoxide
+        eval "$(zoxide init zsh)"
+
+        # enable fzf
+        # Set up fzf key bindings and fuzzy completion
+        eval "$(fzf --zsh)"
+
+        # enable direnv
+        eval "$(direnv hook zsh)"
       '';
     };
 
