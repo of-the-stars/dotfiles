8b3fbdbe0eff60599ca82a1637f55e2a5b6f64ee
of-the-stars <stellae.incaelis@gmail.com>
2026-08-24 13:05:20 -0500

    Creating the live ISO failed + new function
    9 2026-08-24 13:05:18 26.05.20260714.8eeec93 6.18.38
    
    - Switching to the default kernel didn't help with making a live ISO of
      my config
    - Switched to making the home-manager config with a function that takes
      in a user

--- nixos/hosts/han-tyumi/default.nix
+++ nixos/hosts/han-tyumi/default.nix
@@ -5,6 +5,26 @@
 }:
 let
   stellae = users.stellae;
+
+  makeHomeManagerConfig = user: {
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+    home-manager.backupFileExtension = "bak";
+    home-manager.extraSpecialArgs = {
+      inherit
+        inputs
+        user
+        ;
+      inherit (user) username;
+    };
+    home-manager.users.${user.username} = {
+      imports = [
+        user.home
+      ];
+      home.username = "${user.username}";
+      home.homeDirectory = "/home/${user.username}";
+    };
+  };
 in
 {
   specialArgs = {
@@ -13,33 +33,16 @@ in
       stellae
       ;
   };
+
+  # ({ pkgs, modulesPath, ... }: {
+  #   imports = [ (modulesPath + "/installer/cd-dvd/iso-image.nix") ]; # TODO: Fix the module so that it doesn't send me to emergency mode
+  # })
   modules = [
-    # ({ pkgs, modulesPath, ... }: {
-    #   imports = [ (modulesPath + "/installer/cd-dvd/iso-image.nix") ]; TODO: Fix the module so that it doesn't send me to emergency mode cuz it can't mount a partition
-    # })
     ./configuration.nix
     ./hardware-configuration.nix
     # Home manager
     inputs.home-manager.nixosModules.home-manager
-    {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.backupFileExtension = "bak";
-      home-manager.extraSpecialArgs = {
-        inherit
-          inputs
-          stellae
-          ;
-        inherit (stellae) username;
-      };
-      home-manager.users.${stellae.username} = {
-        imports = [
-          stellae.home
-        ];
-        home.username = "${stellae.username}";
-        home.homeDirectory = "/home/${stellae.username}";
-      };
-    }
+    (makeHomeManagerConfig stellae)
     # Veila
     inputs.veila.nixosModules.default
     {
