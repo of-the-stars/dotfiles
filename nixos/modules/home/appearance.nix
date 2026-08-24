0814d709878c05086c622d227696fc3260954226
of-the-stars <stellae.incaelis@gmail.com>
2026-08-24 13:31:25 -0500

    10 2026-08-24 13:31:22 26.05.20260714.8eeec93 6.18.38

--- nixos/modules/home/appearance.nix
+++ nixos/modules/home/appearance.nix
@@ -10,17 +10,39 @@
   };
 
   config = lib.mkIf config.appearance.enable {
-    dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-    dconf.settings."org/gnome/desktop/interface".gtk-theme = "Adwaita-dark";
+    dconf.settings."org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "Adwaita-dark";
+    };
+
     qt = {
       enable = true;
       platformTheme.name = "kde";
       style.name = "breeze";
     };
-    home.file.".config/kdeglobals" = {
-      text = ''
-        ${builtins.readFile "${pkgs.kdePackages.breeze}/share/color-schemes/BreezeDark.colors"}
-      '';
+
+    gtk = {
+      colorScheme = "dark";
+      font = {
+        name = "RobotoMono Nerd Font";
+        package = pkgs.nerd-fonts.roboto-mono;
+      };
+    };
+
+    home = {
+      file.".config/kdeglobals" = {
+        text = ''
+          ${builtins.readFile "${pkgs.kdePackages.breeze}/share/color-schemes/BreezeDark.colors"}
+        '';
+      };
+
+      pointerCursor = {
+        x11.enable = true;
+        gtk.enable = true;
+        package = pkgs.catppuccin-cursors.mochaBlue;
+        name = "Catppuccin-Mocha-Blue-Cursors";
+        size = 48;
+      };
     };
   };
 }
