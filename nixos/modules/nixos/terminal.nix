3fcccf84f40a1e7df5155838b9ae817cec3df77a
of-the-stars <stellae.incaelis@gmail.com>
2026-08-26 12:24:12 -0500

    Added `pandoc` and tweaked opacity rules
    15 2026-08-26 12:24:09 26.05.20260714.8eeec93 6.18.38

--- nixos/modules/nixos/terminal.nix
+++ nixos/modules/nixos/terminal.nix
@@ -56,6 +56,7 @@ in
           lazygit # TUI for `git`
           nmap # Map the network
           openconnect # Connect to VPNs
+          pandoc # Document converter
           rename # Perl rename
           ripgrep # Modern `grep`
           rsync # Sync drives
